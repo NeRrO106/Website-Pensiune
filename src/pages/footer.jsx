64d22304b5e79afa03837c8266da9514c351e58f
NeRrO106 <andreilimit@yahoo.com>
2025-05-26 17:34:06 +0300

    footer update

--- src/pages/footer.jsx
+++ src/pages/footer.jsx
@@ -4,7 +4,7 @@ import './footer.scss';
 const Footer = () => {
     return(
         <footer className="footer">
-            <p>&copy; 2025 Pensiunea Noastra. All rights reserved.</p>
+            <p>&copy; {new Date().getFullYear()} Pensiunea "La Munte". All rights reserved.</p>
             <p>Design și dezvoltare de <a href="https://siteul-tau.com" target="_blank" rel="noopener noreferrer">Candy Studio</a></p>
         </footer>
     );
